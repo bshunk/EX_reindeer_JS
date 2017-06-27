b916acff9d62e1f84aad646f4954e69338905f68
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-27 15:06:19 -0500

    complete reindeer JS

--- reindeer.js
+++ reindeer.js
@@ -21,7 +21,3 @@ for (var i = 0; i < shortestArray.length; i++) {
 
 // Your task is to loop through all the reindeer in the array, and add the name of the reindeer to the single HTML <div> element provided. The name of the reindeer should be prepended with the corresponding color from the other array.
 
-// Example output:
-	// Blue Dasher
-	// Red Dancer
-
